eeed76209dc3aa8f61194cc97fc8ed192bc99adf
tom_glb <goodMorning_pro@atguigu.com>
2019-04-01 18:29:19 +0800

    1111111111111

--- src/com/itcast/nihao1.java
+++ src/com/itcast/nihao1.java
@@ -2,7 +2,7 @@ package com.itcast;
 
 public class nihao1 {
 	public static void main(String[] args) {
-		System.out.println("llloooooo");
+		System.out.println("llloooooo101111111111111111");
 	}
 
 }
